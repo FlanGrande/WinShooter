dd4618abf9da6aa78c34ee3f290743d15258b2b1
FlanGrande <francisjgrande@gmail.com>
2020-04-08 17:38:30 +0100

    Created a bunch of assets.

---
+++ scenes/Environment/Tree.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+export var tree_image : Texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	texture = tree_image
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
