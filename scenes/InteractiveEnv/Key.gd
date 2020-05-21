a89ad133bed0713097ddd3c0ed920136e72a89c2
FlanGrande <francisjgrande@gmail.com>
2020-05-21 20:56:35 +0100

    Added Key sprite with animation
    I also created a script, outside of this project, to generate animated texture resources from a bunch of sprites, to automatize creating animated textures, as I don't feel like drag and dropping each individual frame one by one...

--- scenes/InteractiveEnv/Key.gd
+++ scenes/InteractiveEnv/Key.gd
@@ -3,9 +3,17 @@ extends Node2D
 onready var mechanism = get_parent().get_parent()
 onready var mechanism_id = mechanism.ID
 
+var is_grabbed = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	is_grabbed = false
+
+#func _process(delta):
+#	$Sprite.texture.set_pause(is_grabbed)
+#
+#	if(is_grabbed):
+#		$Sprite.texture.set_current_frame(0)
 
 func use():
 	get_parent().use()
