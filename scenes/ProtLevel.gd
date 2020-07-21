b48085c2fc285174e5681d84d82f98a148ee828d
FlanGrande <francisjgrande@gmail.com>
2020-07-21 01:05:10 +0100

    Adjusted level to new style, part 1.

--- scenes/ProtLevel.gd
+++ scenes/ProtLevel.gd
@@ -16,3 +16,19 @@ func _process(delta):
 func get_a_path(start_position : Vector2, end_position : Vector2):
 	var new_path : = nav_2d.get_simple_path(start_position, end_position)
 	return new_path
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("hewie"):
+		$TileMaps/Roof.visible = false
+		$TileMaps/RoofFront.visible = false
+		body.get_node("LightForEntities").shadow_item_cull_mask = 3 # 3 means shadows are generated for walls.
+		body.get_node("LightForShadows").shadow_item_cull_mask = 3
+		print("Enter")
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("hewie"):
+		$TileMaps/Roof.visible = true
+		$TileMaps/RoofFront.visible = true
+		body.get_node("LightForEntities").shadow_item_cull_mask = 2 # 2 means shadows are NOT generated for walls.
+		body.get_node("LightForShadows").shadow_item_cull_mask = 2
+		print("Exit")
